a76e5798ece1d35bd357eb922a78da1498282cc5
Erik Asi­s <e.asis@identtia.com>
2021-04-29 14:55:08 +0200

    Corregida configuración de estilos

--- config/style.js
+++ config/style.js
@@ -10,8 +10,10 @@ module.exports = function (eleventyConfig, options = {}) {
     // sobreescribimos las opciones por defecto por las opciones proporcionadas por el usuario
     options = {...defaultConfig, ...options};
 
+    const stylesPath = path.join(options.outputPath, "styles");
+
     eleventyConfig.addPlugin(require("eleventy-plugin-sass"), {
-        outputDir: path.join(options.outputPath, "styles"),
+        outputDir: stylesPath,
 
         // indicamos si queremos remapear las rutas
         remap: true,
@@ -21,6 +23,11 @@ module.exports = function (eleventyConfig, options = {}) {
 
         // indicamos si queremos usar el módulo cleanCSS
         cleanCSS: options.isProduction,
+
+        // opciones para pasar a libSass
+        sassOptions: {
+            outputStyle: options.isProduction ? "compressed" : "expanded", // valores posibles: nested, expanded, compact, compressed
+        }
     });
 
 };
