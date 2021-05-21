b775a2e0d9bd1a0bcfbe2ed2ff1cb2c2e02cf725
Erik Asi­s <e.asis@identtia.com>
2021-05-21 11:48:13 +0200

    Eliminado import innecesario en la configuración de estilos

--- config/style.js
+++ config/style.js
@@ -1,5 +1,4 @@
 const path = require("path");
-const sass = require('sass');
 
 const defaultConfig = {
     rootPath: "./",
@@ -14,7 +13,6 @@ module.exports = function (eleventyConfig, options = {}) {
     options = { ...defaultConfig, ...options };
 
     const nodeModulesPath = path.resolve(options.rootPath, "node_modules");
-    console.log("style", nodeModulesPath);
 
     eleventyConfig.on("beforeBuild", () => {
         // Custom Code
